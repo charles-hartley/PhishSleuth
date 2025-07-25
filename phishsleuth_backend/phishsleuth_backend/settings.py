66f80c253a931235a3325f48b839ba751dc04a32
charles-mutuku <kyalucharles14@gmail.com>
2025-07-25 11:33:09 +0300

    latest updates

--- phishsleuth_backend/phishsleuth_backend/settings.py
+++ phishsleuth_backend/phishsleuth_backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%3h^*u2zt6ok*r6od#yx(yywz%c7jsxcv-ho*-zjgc+3)prl^s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
